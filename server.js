e44dafd173673a779b8e68713792677aca90196d
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-02-16 19:01:18 +0000

    simple get and post requests added to the server

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var app = express()
+var app = express();
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    console.log('Something is happening');
+    next();
+});
+    /*GET method*/
+app.get('/', function (req, res) {
+  //res.send('GET request success');
+  //or
+  console.log('GET request success');
+  res.json({ message: 'JSON GET request success' });
+})
+
+    /*POST method*/
+app.post('/', function (req, res) {
+  //res.send('POST request success');
+  //or
+  console.log('POST request success');
+  res.json({ message: 'JSON POST request success' });
+})
 
 app.listen(3000, function () {
     console.log('Server listening on', 3000);
