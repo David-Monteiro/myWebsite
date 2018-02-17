2e943069b28f5737499281b443449ba48357255d
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-02-17 18:00:46 +0000

    update to server.js

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ app.use(function(req, res, next) {
     next();
 });
 
+var Post = require('./models/post');
+
 ////////////Simple//Requests/////////////
 
-var Post = require('.models/post');
     /*GET method*/
 app.get('/', function (req, res) {
   //res.send('GET request success');
@@ -28,8 +29,35 @@ app.post('/', function (req, res) {
   res.json({ message: 'JSON POST request success' });
 })
 
-//////////////////////////////////////
+//////////Requests//for//Database////////
 
+    /*POST method*/
+var Post = require('./models/post');
+app.post('/api/posts', function (req, res, next) {
+    var post = new Post({
+        username: req.body.username,
+        body: req.body.body
+    });
+    post.save(function (err, post) {
+        if(err) { return next(err) }
+        res.json(201, post)
+    });
+    console.log('POST request success');
+});
+
+    /*GET method*/
+app.get('/api/posts', function (req, res, next) {
+    Post.find( function(err, posts) {
+        if(err) { return next(err); }
+        res.json(posts);
+    });
+    console.log('GET request success');
+});
+
+
+
+
+/////////////////////////////////////////
 app.listen(3000, function () {
     console.log('Server listening on', 3000);
 })
