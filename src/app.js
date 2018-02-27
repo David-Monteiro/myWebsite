2fc2e812394c9f687a32be0c2c7787672dce063a
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-02-27 20:56:03 +0000

    Angular code cleaning

---
+++ src/app.js
@@ -0,0 +1,44 @@
+var app = angular.module('app', []);
+
+app.controller('PostsCtrl', function($scope, PostSvc){
+
+	$scope.addPost = function(){		
+
+		if($scope.postBody) {
+			var post = {
+				username : "david",
+				body : $scope.postBody
+			};
+			console.log("POST: 1st step");
+			PostSvc.create(post)
+			.then( function(data, status, headers, config) {
+				$scope.getPosts();
+				console.log("POST: success");
+			}, 
+			function(data, status, headers, config) {
+				console.error('POST: Error');
+			});	
+				
+			// Making the field empty
+			$scope.postBody=null;
+		}
+	};
+
+	$scope.getPosts = function(){
+		PostSvc.fetch()
+		  .then(function(posts){ $scope.posts = posts.data; }, 
+		  function(data, status, headers, config) {
+			console.log('GET: Error');
+		});
+	};
+
+});
+
+app.service('PostSvc', function ($http) {
+	this.fetch = function() {
+		return $http.get('/api/posts');
+	};
+	this.create = function(post) {
+		return $http.post('/api/posts', post);
+	};
+});
