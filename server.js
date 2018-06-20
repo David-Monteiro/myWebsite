4878733c622f2f72c710d58c3358abc0b6052d6a
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-06-20 21:30:50 +0100

    users api

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(require('./routes/api/posts'));
 app.use(require('./routes/static'));
 app.use('/api/sessions',
     require('/controller/api/sessions'));
+app.use('/api/users',
+    require('/controller/api/users'));
 
 
 app.listen(3000, function () {
