824743aea4d0d026fa44bf3f12e10d7bd7531177
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-06-21 22:58:51 +0100

    update to sessions

--- routes/api/sessions.js
+++ routes/api/sessions.js
@@ -1,8 +1,3 @@
-app.use('/api/sessions',
-    require('/controller/api/sessions'));
-//
-//
-// create a session conroller at controllers/api/sessions.js
 var router = require('express').Router();
 var User = require('../../models/user');
 var bcrypt = require('bcrypt');
@@ -10,21 +5,17 @@ var jwt = require('jwt-simple');
 var config = require('../../config');
 
 router.post('/', function(req, res, next) {
-    User.findOne({
-        username: req.boddy.username
-    }).select('password').select('username')
-    .exec(function(err, user) {
-        if(err) { return next(err) }
-        if(!user) { return res.send(401) }
-        bcrypt.compare(req.body.password, user.password, function (err, valid) {
-            if(err) { return next(err) }
+    User.findOne({ username: req.body.username },
+        function(err, user) {
+            if (err) console.log("error");
             if(!user) { return res.send(401) }
-            var token = jwt.encode(
-            {
-                username: user.username, 
-                config.secret
-            });
-            res.send(token);
-        });
-    });
+                bcrypt.compare(req.body.password, user.password, function(err, valid) {
+                    if(err || !valid) {
+                        if(!user) { return res.send(401) }
+                        return res.status(401).json("not a match");
+                    } 
+                    var token = jwt.encode({username: user.username}, secretKey);
+                    return res.status(201).json(token);
+            })
+        })
 });
