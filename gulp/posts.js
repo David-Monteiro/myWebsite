05d9af3037e2d8448cbd68aa768f5a0236c75a93
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-07-13 21:45:20 +0100

    gulp posts build

--- gulp/posts.js
+++ gulp/posts.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var htmlMinify = require("gulp-html-minify");
 
 gulp.task('posts', function () {
-    gulp.src('./src/app/components/login/login.html')
+    gulp.src('src/app/components/posts/posts.html')
     .pipe(htmlMinify())
     .pipe(gulp.dest('public'));
     console.log("posts: gulp");
 });
 
 gulp.task('watch:posts', ['posts'], function(){
-	gulp.watch('src/app/components/login/login.html', ['posts'])
+	gulp.watch('src/app/components/posts/posts.html', ['posts'])
 	console.log("watch posts: gulp");
 });
