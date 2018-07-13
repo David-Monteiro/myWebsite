bc4d8d9007077ac379ad17b3c786527b0ceb987d
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-07-13 21:50:20 +0100

    gulp html file comments for testing reasons

--- gulp/html.js
+++ gulp/html.js
@@ -5,8 +5,10 @@ gulp.task('html', function () {
     gulp.src('./src/index.html')
     .pipe(htmlMinify())
     .pipe(gulp.dest('public'));
+    console.log("html: gulp");
 });
 
 gulp.task('watch:html', ['html'], function(){
 	gulp.watch('src/index.html', ['html'])
+	console.log("watch html: gulp");
 });
