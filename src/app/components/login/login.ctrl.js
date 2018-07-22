4c2877466468e6c8a7d7f23a4536732530c73f56
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-07-22 22:49:58 +0100

    login components code improvement

--- src/app/components/login/login.ctrl.js
+++ src/app/components/login/login.ctrl.js
@@ -1,8 +1,9 @@
 angular.module('app')
-.controller('LoginCtrl', function($scope, LoginSvc) {
+.controller('LoginCtrl', function($scope, UserSvc) {
     $scope.login = function(username, password){
         LoginSvc.login(username, password)
             .then(function(user){
+            	$scope.$emit('login', user.data);
                 console.log(user);
         })
     }
