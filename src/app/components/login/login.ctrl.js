c09d905af218b03cbbc77c1c43e2949eda5975be
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-07-23 22:30:30 +0100

    user service code adjustment

--- src/app/components/login/login.ctrl.js
+++ src/app/components/login/login.ctrl.js
@@ -1,7 +1,7 @@
 angular.module('app')
 .controller('LoginCtrl', function($scope, UserSvc) {
     $scope.login = function(username, password){
-        LoginSvc.login(username, password)
+        UserSvc.login(username, password)
             .then(function(user){
             	$scope.$emit('login', user.data);
                 console.log(user);
