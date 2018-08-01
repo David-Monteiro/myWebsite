1b2fe6954d080057c164ad9769e55f1d83ce1300
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-08-01 22:12:21 +0100

    post updated to better implement pulled content and removed from blogs module

--- src/app/components/post/post.dir.js
+++ src/app/components/post/post.dir.js
@@ -1,6 +1,14 @@
 angular.module('app').directive('postSection', function(){
 	return {
 		restrict: "E",
+		scope: {
+			username : '@',
+			title : '@',
+			body : '@',
+			tags : '@',
+			date : '@'
+		},
 		templateUrl: "templates/post.html"
+		
 	};
 });
