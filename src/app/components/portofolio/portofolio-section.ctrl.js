586ee63c50ac22c64f7f52fe7df2e4eb16421562
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-09-09 21:45:59 +0100

    aditional changes made to portofolio to better suit different screen sizes

--- src/app/components/portofolio/portofolio-section.ctrl.js
+++ src/app/components/portofolio/portofolio-section.ctrl.js
@@ -34,21 +34,36 @@ var myProjects = [
 ];
 
 angular.module('app')
-.controller('portofolioCtrl', function($scope) {
+.controller('portofolioCtrl', function($scope, $window) {
 	$scope.projects = myProjects;
     $scope.panel = -1;
 	$scope.hover = false;
 	$scope.cardHover = [false , false, false, false ];
 	$scope.hovering = "'opacity': '0.5'";
+	$scope.panelOpen = false;
 	
 	$scope.selectPanel = function(setPanel) {
 		console.log("asda");
-		if($scope.panel === setPanel) $scope.panel = -1;
-		else $scope.panel = setPanel;
+		if($scope.panel === setPanel) {
+			$scope.panel = -1; 
+			$scope.panelOpen = false;
+		} 
+		else {
+			$scope.panel = setPanel; 
+			$scope.panelOpen = true; 
+		}
 	}
 	
 	$scope.isSelected = function(checkPanel) {
 		return $scope.panel === checkPanel
 	}
+
+	$scope.setWindowSize = function() {
+		$scope.windowWidth = $window.innerWidth;
+	}
+
+	angular.element($window).bind('resize', function(){
+		$scope.windowWidth = $window.innerWidth;
+	}); 
 
 });
