60f3eeab4bcc09d38c96cb70361a4d645f58b97d
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-10-10 23:22:31 +0100

    new images added to the site

--- src/app/components/homePage/portofolio/portofolio-section.ctrl.js
+++ src/app/components/homePage/portofolio/portofolio-section.ctrl.js
@@ -5,7 +5,8 @@ var myProjects = [
 		hero: "Project 1",
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis, augue ac fermentum volutpat, ipsum eros vehicula nisi, eu ullamcorper ex ante non elit. Pellentesque eu nisi venenatis, gravida tortor non, mollis mauris.",
 		link: "https://github.com/David-Monteiro/myWebsite",
-		image: "http://headerart.weebly.com/uploads/5/7/5/7/5757212/eye-catching-colorful-paints-art-header_orig.jpg"
+		//image: "http://headerart.weebly.com/uploads/5/7/5/7/5757212/eye-catching-colorful-paints-art-header_orig.jpg"
+		image: "images/patrick-hendry-723641-unsplash.jpg"
 	}, 
 	{
 		name: "project 2",
@@ -13,7 +14,8 @@ var myProjects = [
 		hero: "Project 2",
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis, augue ac fermentum volutpat, ipsum eros vehicula nisi, eu ullamcorper ex ante non elit. Pellentesque eu nisi venenatis, gravida tortor non, mollis mauris.",
 		link: "https://github.com/David-Monteiro/myWebsite",
-		image: "http://headerart.weebly.com/uploads/5/7/5/7/5757212/4495832_orig.jpg"
+		//image: "http://headerart.weebly.com/uploads/5/7/5/7/5757212/4495832_orig.jpg"
+		image: "images/roman-mager-59976-unsplash.jpg"
 	},
 	{
 		name: "project 3",
@@ -21,7 +23,8 @@ var myProjects = [
 		hero: "Project 3",
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis, augue ac fermentum volutpat, ipsum eros vehicula nisi, eu ullamcorper ex ante non elit. Pellentesque eu nisi venenatis, gravida tortor non, mollis mauris.",
 		link: "https://github.com/David-Monteiro/myWebsite",
-		image: "http://headerart.weebly.com/uploads/5/7/5/7/5757212/5832164_orig.jpg"
+		//image: "http://headerart.weebly.com/uploads/5/7/5/7/5757212/5832164_orig.jpg"
+		image: "images/pavan-trikutam-1660-unsplash.jpg"
 	},
 	{
 		name: "project 4",
@@ -29,7 +32,8 @@ var myProjects = [
 		hero: "Project 4",
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus facilisis, augue ac fermentum volutpat, ipsum eros vehicula nisi, eu ullamcorper ex ante non elit. Pellentesque eu nisi venenatis, gravida tortor non, mollis mauris.",
 		link: "https://github.com/David-Monteiro/myWebsite",
-		image: "http://headerart.weebly.com/uploads/5/7/5/7/5757212/6370839_orig.jpg"
+		//image: "http://headerart.weebly.com/uploads/5/7/5/7/5757212/6370839_orig.jpg"
+		image: "images/aziz-acharki-549137-unsplash.jpg"
 	}
 ];
 
