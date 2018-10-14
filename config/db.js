660adc6e373ad163f9ee8e051009862dc70ea42a
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-10-14 22:05:02 +0100

    db update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
-var url = process.env. + "mongodb://heroku_snm82h11:3tn8g23tli7vf86eg2k3ecnp47@ds131973.mlab.com:31973/heroku_snm82h11" || 'mongodb://localhost/blogs'
+var url = process.env.process.env.MONGODB_URI || 'mongodb://localhost/blogs'
 
-mongoose.connect('mongodb://localhost/blogs',
+mongoose.connect(url,
     function() {
         console.log('mongodb connected');
     }
