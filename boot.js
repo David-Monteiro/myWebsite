ab37797e46d8c8919e106778d65aaa32376f7c6c
David Monteiro <david.monteiro2@mail.dcu.ie>
2018-10-14 21:59:52 +0100

    dir change and db update

--- boot.js
+++ boot.js
@@ -1,6 +1,6 @@
 var cluster = require('cluster');
 var numCpus = require('os').cpus().length;
-cluster.setupMaster({exec: path.resolve(__dirname + '/../server.js')});
+cluster.setupMaster({exec: __dirname + '/server.js'});
 for(var i = 0; i<numCpus; i++){
 	cluster.fork();
 }
--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose')
+var url = process.env. + "mongodb://heroku_snm82h11:3tn8g23tli7vf86eg2k3ecnp47@ds131973.mlab.com:31973/heroku_snm82h11" || 'mongodb://localhost/blogs'
+
 mongoose.connect('mongodb://localhost/blogs',
     function() {
         console.log('mongodb connected');
